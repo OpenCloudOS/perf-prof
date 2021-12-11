1bed43b42b5d3e32b6e4743a28438fdc42b66862
yongduan <yongduan@tencent.com>
2021-12-11 23:17:19 +0800

    monitor: add kvm-exit, monitor kvm_exit to kvm_entry latency
    Usage:
    ^C     kvm-exit latency(ns) : count    distribution
             0 -> 1          : 0        |                                        |
             2 -> 3          : 0        |                                        |
             4 -> 7          : 0        |                                        |
             8 -> 15         : 0        |                                        |
            16 -> 31         : 0        |                                        |
            32 -> 63         : 0        |                                        |
            64 -> 127        : 0        |                                        |
           128 -> 255        : 0        |                                        |
           256 -> 511        : 30       |*                                       |
           512 -> 1023       : 627      |****************************************|
          1024 -> 2047       : 28       |*                                       |
          2048 -> 4095       : 1        |                                        |
    
    Signed-off-by: duanery <corcpp@foxmail.com>
    Signed-off-by: yongduan <yongduan@tencent.com>

--- monitor.c
+++ monitor.c
@@ -67,6 +67,7 @@ const char argp_program_doc[] =
 "    perf-monitor watchdog [-F freq] [-g] [-m pages] [-C cpu] [-v]\n"
 "    perf-monitor kmemleak --alloc tp --free tp [-m pages] [-g] [-v]\n"
 "    perf-monitor percpu-stat -i INT [-C cpu] [--syscalls]\n"
+"    perf-monitor kvm-exit [-C cpu]\n"
 "\n"
 "EXAMPLES:\n"
 "    perf-monitor split-lock -T 1000 -C 1-21,25-46 -G  # Monitor split-lock\n"
