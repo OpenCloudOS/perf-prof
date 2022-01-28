1abeda2298ac985ccdd07f2b269882fa98c152be
yongduan <yongduan@tencent.com>
2022-01-28 11:46:50 +0800

    stack_helpers: stack related functions
    Introduce callchain_ctx to gather symbol information.
    callchain_ctx_init, callchain_ctx_deinit, print_callchain.
    
    Signed-off-by: duanery <corcpp@foxmail.com>
    Signed-off-by: yongduan <yongduan@tencent.com>

---
+++ stack_helpers.h
@@ -0,0 +1,13 @@
+#ifndef __STACK_HELPERS_H
+#define __STACK_HELPERS_H
+
+struct callchain {
+    __u64   nr;
+    __u64   ips[0];
+};
+
+int callchain_ctx_init(bool kernel, bool user);
+void callchain_ctx_deinit(bool kernel, bool user);
+void print_callchain(FILE *f, struct callchain *callchain, u32 pid);
+
+#endif
