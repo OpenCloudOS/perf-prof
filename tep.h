4738d3570aa25d37f5a643c759d1a5b72d14dfee
duanery <corcpp@foxmail.com>
2022-02-12 16:57:44 +0800

    mpdelay: multiple event parsing abstracted into tp_list_new
    Signed-off-by: duanery <corcpp@foxmail.com>

--- tep.h
+++ tep.h
@@ -13,6 +13,28 @@ void tep__print_event(unsigned long long ts, int cpu, void *data, int size);
 
 void monitor_tep__comm(union perf_event *event, int instance);
 
+struct tp {
+    struct perf_evsel *evsel;
+    int id;
+    char *sys;
+    char *name;
+    char *filter;
+    int stack;
+    int max_stack;
+};
+
+struct tp_list {
+    int nr_tp;
+    int nr_need_stack;
+    bool need_stream_id;
+    struct tp tp[0];
+};
+
+struct tp_list *tp_list_new(char *event);
+void tp_list_free(struct tp_list *tp_list);
+
+
+
 
 #endif
 
