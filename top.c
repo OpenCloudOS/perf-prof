1e0ad4f09ab2738104eb314b87c8f76bb00974f6
yongduan <yongduan@tencent.com>
2022-02-27 00:54:28 +0800

    top: support for multiple events and fields
    Associate the event to the process and display it in the top mode, and
    you can customize multiple events and their fields to be observed.
    
    Processes have many parameters that are not exported in the proc/<pid>/
    directory. We can use the fields exported by the kernel tracepoint, as
    well as dynamically added fields via kprobe, to observe this information
    of the process in the top mode.
    
    The event attribute `top-add' adds a top column, and `top-by' adds a
    sorted top column. Assign the field name of the tracepoint.
    
    Run cat command to see all fields.
    $ cat /sys/kernel/debug/tracing/events/sched/sched_stat_wait/format
    
    Example
    $ sysctl kernel.sched_schedstats=1  #enable schedstats
    $ perf-prof top -e "sched:sched_stat_iowait//top-add=delay/,sched:sched_stat_sleep//top-add=delay/,sched:sched_stat_wait//top-by=delay/"
    perf-prof - 00:36:46  sample 1989797 events
         PID        DELAY        DELAY        DELAY COMM
      127466            0   2545278377    158442485 pal_session
      141483            0   2880332128    104786645 python
    
    PID  DELAY  DELAY  DELAY  COMM
         |      |      `sched:sched_stat_wait delay field, time the task is runnable
         |      |       but not actually running due to scheduler contention
         |      `sched:sched_stat_sleep delay field, time the task is not runnable
         |       including iowait
         `sched:sched_stat_iowait delay field, time the task is not runnable due to
          waiting on IO to complete
    
    Signed-off-by: duanery <corcpp@foxmail.com>
    Signed-off-by: yongduan <yongduan@tencent.com>

--- top.c
+++ top.c
@@ -6,7 +6,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/time.h>
+#include <time.h>
 #include <signal.h>
+#include <linux/compiler.h>
 #include <termios.h>
 #include <monitor.h>
 #include <dlfcn.h>
@@ -24,8 +27,16 @@ static struct monitor_ctx {
     struct tp_list *tp_list;
     struct rblist pid_list;
     unsigned long nr_events;
-    char *name;
-    int name_len;
+
+    char *EVENT; //toupper(env->event)
+    int nr_fields;
+    int nr_top_by;
+    struct {
+        char *field;
+        int len;
+        bool top_by;
+    } *fields;
+
     int ws_row;
     int ws_col;
     bool tty;
@@ -36,7 +47,7 @@ struct top_info {
     struct rb_node rbnode;
     int pid;
     char comm[TASK_COMM_LEN];
-    unsigned long long counter;
+    unsigned long long counter[0];
 };
 
 static int top_info_node_cmp(struct rb_node *rbn, const void *entry)
@@ -54,10 +65,12 @@ static int top_info_node_cmp(struct rb_node *rbn, const void *entry)
 
 static struct rb_node *top_info_node_new(struct rblist *rlist, const void *new_entry)
 {
+    int size = offsetof(struct top_info, counter[ctx.nr_fields]);
     const struct top_info *e = new_entry;
-    struct top_info *t = malloc(sizeof(struct top_info));
+    struct top_info *t = malloc(size);
     if (t) {
         memcpy(t, e, sizeof(struct top_info));
+        memset((void *)t + sizeof(struct top_info), 0, size - sizeof(struct top_info));
         RB_CLEAR_NODE(&t->rbnode);
         return &t->rbnode;
     } else
@@ -75,13 +88,26 @@ static int top_info_sorted_node_cmp(struct rb_node *rbn, const void *entry)
 {
     struct top_info *t = container_of(rbn, struct top_info, rbnode);
     const struct top_info *e = entry;
+    int i;
 
-    if (t->counter > e->counter)
-        return -1;
-    else if (t->counter < e->counter)
-        return 1;
-    else
-        return 0;
+    if (ctx.nr_top_by)
+    for (i = 0; i < ctx.nr_fields; i++) {
+        if (!ctx.fields[i].top_by)
+            continue;
+        if (t->counter[i] > e->counter[i])
+            return -1;
+        else if (t->counter[i] < e->counter[i])
+            return 1;
+    }
+    for (i = 0; i < ctx.nr_fields; i++) {
+        if (ctx.fields[i].top_by)
+            continue;
+        if (t->counter[i] > e->counter[i])
+            return -1;
+        else if (t->counter[i] < e->counter[i])
+            return 1;
+    }
+    return top_info_node_cmp(rbn, entry);
 }
 static struct rb_node *top_info_sorted_node_new(struct rblist *rlist, const void *new_entry)
 {
@@ -112,28 +138,41 @@ static void sig_winch(int sig)
 
 static int monitor_ctx_init(struct env *env)
 {
-    int i;
+    int i, j, f = 0;
+    int len;
 
     if (!env->event)
         return -1;
 
     tep__ref();
 
+    ctx.EVENT = strdup(env->event);
+    len = strlen(ctx.EVENT);
+    for (i = 0; i < len; i++)
+        ctx.EVENT[i] = (char)toupper(ctx.EVENT[i]);
+
     ctx.tp_list = tp_list_new(env->event);
     if (!ctx.tp_list)
         return -1;
 
-    if (ctx.tp_list->nr_tp > 1) {
-        fprintf(stderr, "More than one event\n");
+    ctx.nr_fields = ctx.tp_list->nr_top;
+    ctx.fields = calloc(ctx.nr_fields, sizeof(*ctx.fields));
+    if (!ctx.fields)
         return -1;
+    for (i = 0; i < ctx.tp_list->nr_tp; i++) {
+        struct tp *tp = &ctx.tp_list->tp[i];
+        for (j = 0; j < tp->nr_top; j++) {
+            ctx.fields[f].field = ctx.EVENT + (tp->top_add[j].field - env->event);
+            ctx.fields[f].len = strlen(tp->top_add[j].field);
+            ctx.fields[f].field[ctx.fields[f].len] = '\0';
+            ctx.fields[f].top_by = tp->top_add[j].top_by;
+            if (ctx.fields[f].len < 12)
+                ctx.fields[f].len = 12;
+            if (ctx.fields[f].top_by)
+                ctx.nr_top_by ++;
+            f ++;
+        }
     }
-
-    ctx.name = strdup(ctx.tp_list->tp[0].top_by ?: ctx.tp_list->tp[0].name);
-    ctx.name_len = strlen(ctx.name);
-    for (i = 0; i < ctx.name_len; i++)
-        ctx.name[i] = (char)toupper(ctx.name[i]);
-    if (ctx.name_len < 12)
-        ctx.name_len = 12;
 
     rblist__init(&ctx.pid_list);
     ctx.pid_list.node_cmp = top_info_node_cmp;
@@ -147,7 +186,7 @@ static int monitor_ctx_init(struct env *env)
         sig_winch(SIGWINCH);
         signal(SIGWINCH, sig_winch);
         set_term_quiet_input(&save);
-        printf("\033[?1049h\033[H\033[2J\033[?25l\n");
+        printf("\033[?1049h\033[?25l\033[H\033[2J\n");
     }
 
     if (env->interval == 0)
@@ -161,9 +200,11 @@ static void monitor_ctx_exit(void)
 {
     if (ctx.tty) {
         tcsetattr(0, TCSANOW, &save);
-        printf("\033[?1049l\033[?25h");
+        printf("\033[?25h\n\033[?1049l");
     }
     rblist__exit(&ctx.pid_list);
+    free(ctx.fields);
+    free(ctx.EVENT);
     tp_list_free(ctx.tp_list);
     tep__unref();
 }
@@ -242,21 +283,27 @@ struct sample_type_raw {
     __u64		period;
     struct {
         __u32   size;
-        __u8    data[0];
+        union {
+            __u8    data[0];
+            unsigned short common_type;
+        } __packed;
     } raw;
 };
 
 static void top_sample(union perf_event *event, int instance)
 {
     struct sample_type_raw *raw = (void *)event->sample.array;
+    unsigned short common_type = raw->raw.common_type;
     void *data = raw->raw.data;
     int size = raw->raw.size;
+    struct tp *tp = NULL;
+    int field = 0;
     struct tep_record record;
     struct tep_handle *tep;
     struct trace_seq s;
     struct tep_event *e;
     char *comm;
-    int len;
+    int len, i;
     struct top_info info;
     struct rb_node *rbn;
     struct top_info *p;
@@ -271,6 +318,18 @@ static void top_sample(union perf_event *event, int instance)
         tep__print_event(raw->time/1000, raw->cpu_entry.cpu, data, size);
     }
 
+    for (i = 0; i < ctx.tp_list->nr_tp; i++) {
+        struct tp *tmp = &ctx.tp_list->tp[i];
+        if (common_type == tmp->id) {
+            tp = tmp;
+            break;
+        } else
+            field += tmp->nr_top;
+    }
+
+    if (tp == NULL)
+        goto unref;
+
     trace_seq_init(&s);
 
     memset(&record, 0, sizeof(record));
@@ -283,25 +342,42 @@ static void top_sample(union perf_event *event, int instance)
     if (tep_get_field_val(&s, e, "pid", &record, (unsigned long long *)&info.pid, 0) < 0) {
         info.pid = raw->tid_entry.tid;
     }
-    if (!ctx.tp_list->tp[0].top_by ||
-        tep_get_field_val(&s, e, ctx.tp_list->tp[0].top_by, &record, &info.counter, 0) < 0) {
-        info.counter = 1;
-    }
 
     rbn = rblist__find(&ctx.pid_list, &info);
     if (rbn == NULL) {
         comm = tep_get_field_raw(&s, e, "comm", &record, &len, 0);
         strncpy(info.comm, comm?:tep__pid_to_comm(raw->tid_entry.tid), sizeof(info.comm));
-        rblist__add_node(&ctx.pid_list, &info);
-    } else {
-        p = container_of(rbn, struct top_info, rbnode);
-        p->counter += info.counter;
+        rbn = rblist__findnew(&ctx.pid_list, &info);
+        if (rbn == NULL)
+            goto destroy;
+    }
+
+    p = container_of(rbn, struct top_info, rbnode);
+
+    for (i = 0; i < tp->nr_top; i++, field++) {
+        unsigned long long counter;
+        if (!tp->top_add[i].event &&
+            tep_get_field_val(&s, e, tp->top_add[i].field, &record, &counter, 0) == 0) {
+            p->counter[field] += counter;
+        } else
+            p->counter[field] += 1;
     }
 
     ctx.nr_events ++;
 
+destroy:
     trace_seq_destroy(&s);
+unref:
     tep__unref();
+}
+
+static inline int top_print_time(const char *fmt)
+{
+    char timebuff[64];
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    strftime(timebuff, sizeof(timebuff), "%H:%M:%S", localtime(&tv.tv_sec));
+    return printf(fmt, timebuff);
 }
 
 static void top_interval(void)
@@ -311,6 +387,7 @@ static void top_interval(void)
     struct rblist sorted;
     int row = 3;
     int printed;
+    int i;
 
     if (rblist__empty(&ctx.pid_list))
         return;
@@ -334,10 +411,18 @@ static void top_interval(void)
     else {
         printf("\033[H\033[2J\033[?25h\033[7m");
     }
-    printed = printf("Samples: %lu of event '%s', NR %u", ctx.nr_events, ctx.tp_list->tp[0].name, rblist__nr_entries(&sorted));
-    printf("%*s\n", ctx.tty?ctx.ws_col-printed:0, "");
-    printed = printf("%8s %*s %-*s", "PID", ctx.name_len, ctx.name, TASK_COMM_LEN, "COMM");
-    printf("%*s\n", ctx.tty?ctx.ws_col-printed:0, "");
+
+    printed = top_print_time("perf-prof - %s  ");
+    printed += printf("sample %lu events", ctx.nr_events);
+    printf("%*s\n", ctx.tty && ctx.ws_col>printed ? ctx.ws_col-printed : 0, "");
+
+    // PID FIELD FIELD ... COMM
+    printed = printf("%8s ", "PID");
+    for (i = 0; i < ctx.nr_fields; i++)
+        printed += printf("%*s ", ctx.fields[i].len, ctx.fields[i].field);
+    printed += printf("%-*s", TASK_COMM_LEN, "COMM");
+    printf("%*s\n", ctx.tty && ctx.ws_col>printed ? ctx.ws_col-printed : 0, "");
+
     if (ctx.tty) printf("\033[0m");
 
     do {
@@ -345,7 +430,10 @@ static void top_interval(void)
 
         if (!ctx.tty || !ctx.ws_row || ++row < ctx.ws_row) {
             t = container_of(rbn, struct top_info, rbnode);
-            printf("%8d %*llu %-s\n", t->pid, ctx.name_len, t->counter, t->comm);
+            printf("%8d ", t->pid);
+            for (i = 0; i < ctx.nr_fields; i++)
+                printf("%*llu ", ctx.fields[i].len, t->counter[i]);
+            printf("%-s\n", t->comm);
         }
 
         rblist__remove_node(&sorted, rbn);
