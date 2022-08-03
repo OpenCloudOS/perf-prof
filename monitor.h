dc15c877bcd8cd2a7bc2b037b176fbe5e804e8ef
yongduan <yongduan@tencent.com>
2022-08-03 21:37:47 +0800

    perf-prof: Add support for ebpf filter.
    Currently the filter supports 3 variables:
    - irqs_disabled, filter based on interrupts on and off.
    - nr_running, filter based on the number of runnable processes on the current
      CPU runqueue.
    - tif_need_resched, filter based on whether the TIF_NEED_RESCHED flag is set.
    
    ebpf is not compatible with all kernel versions. To use this feature in perf
    -prof, you need to compile with the CONFIG_LIBBPF=y configuration turned on.
    
    $ make CONFIG_LIBBPF=y
    
    Signed-off-by: duanery <corcpp@foxmail.com>
    Signed-off-by: yongduan <yongduan@tencent.com>

--- monitor.h
+++ monitor.h
@@ -172,4 +172,20 @@ void reduce_wakeup_times(profiler *p, struct perf_event_attr *attr);
 void common_help(struct help_ctx *ctx, bool enabled, bool cpus, bool pids, bool interval, bool order, bool pages, bool verbose);
 
 
+// filter.c
+struct perf_event_filter {
+    void *obj;
+    int perf_event_prog_fd;
+    // args
+    bool filter_irqs_disabled;
+    bool irqs_disabled;
+    bool filter_nr_running;
+    u32 nr_running_min;
+    u32 nr_running_max;
+    bool filter_tif_need_resched;
+    bool tif_need_resched;
+};
+int perf_event_filter_open(struct perf_event_filter *filter);
+void perf_event_filter_close(struct perf_event_filter *filter);
+
 #endif
