fcb344649a4a5046a1a88003d53a0efb341ba772
duanery <corcpp@foxmail.com>
2023-02-08 14:44:24 +0800

    tests: test_top: Added attr expression test cases.
    Signed-off-by: duanery <corcpp@foxmail.com>

--- tests/test_top.py
+++ tests/test_top.py
@@ -60,7 +60,27 @@ def test_sched_stat_runtime(runtime, memleak_check):
 
 def test_sched_process_exec(runtime, memleak_check):
     #perf-prof top -e sched:sched_process_exec//comm=filename/ --only-comm
-    top = PerfProf(['top', '-e', 'sched:sched_process_exec//comm=filename/', '--only-comm'])
+    top = PerfProf(['top', '-e', 'sched:sched_process_exec//comm="(char *)&common_type+filename_offset"/', '--only-comm'])
+    for std, line in top.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
+
+def test_workqueue_execute_start(runtime, memleak_check):
+    #perf-prof top -e 'workqueue:workqueue_execute_start//key=common_pid/alias=NUM/comm=ksymbol(function)/' --only-comm
+    top = PerfProf(['top', '-e', 'workqueue:workqueue_execute_start//key=common_pid/alias=NUM/comm=ksymbol(function)/', '--only-comm'])
+    for std, line in top.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
+
+def test_3event(runtime, memleak_check):
+    #perf-prof top -e sched:sched_switch//key=prev_pid/comm=prev_comm/,sched:sched_wakeup//key=pid/comm=comm/,sched:sched_stat_runtime//top-by="runtime/1000"/alias=run(us)/ -m 64
+    top = PerfProf(['top', '-e', 'sched:sched_switch//key=prev_pid/comm=prev_comm/,sched:sched_wakeup//key=pid/comm=comm/,sched:sched_stat_runtime//top-by="runtime/1000"/alias=run(us)/', '-m', '64'])
     for std, line in top.run(runtime, memleak_check):
         if not memleak_check or (
             std == PerfProf.STDERR and not PerfProf.lost_events(line)):
