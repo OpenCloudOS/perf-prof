67098af7d82c4182c03890f9cfc7bc827b787d5c
duanery <corcpp@foxmail.com>
2023-02-08 15:29:51 +0800

    tests: test_num-dist: Added num attr expression test cases.
    Signed-off-by: duanery <corcpp@foxmail.com>

--- tests/test_num-dist.py
+++ tests/test_num-dist.py
@@ -18,6 +18,15 @@ def test_num_dist_sched_stat_runtime(runtime, memleak_check):
         if not memleak_check:
             assert std == PerfProf.STDOUT
 
+def test_num_dist_sched_stat_runtime_us(runtime, memleak_check):
+    num_dist = PerfProf(['num-dist', '-e', 'sched:sched_stat_runtime//num="runtime/1000"/alias=sched_stat_runtime(us)/', '-i', '1000'])
+    for std, line in num_dist.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
+
 def test_num_dist_sched_stat_blocked(runtime, memleak_check):
     num_dist = PerfProf(['num-dist', '-e', 'sched:sched_stat_blocked//num=delay/', '-i', '1000'])
     for std, line in num_dist.run(runtime, memleak_check):
@@ -60,6 +69,19 @@ def test_num_dist_sched_vmf_rundelay(runtime, memleak_check):
         pytest.skip("'sched:sched_vmf_rundelay' does not exist")
 
     num_dist = PerfProf(['num-dist', '-e', 'sched:sched_vmf_rundelay//num=rundelay/', '-i', '1000'])
+    for std, line in num_dist.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
+
+def test_num_dist_sched_vmf_rundelay_us(runtime, memleak_check):
+    exist, _ = PerfProf.tracepoint_exists('sched:sched_vmf_rundelay')
+    if not exist:
+        pytest.skip("'sched:sched_vmf_rundelay' does not exist")
+
+    num_dist = PerfProf(['num-dist', '-e', 'sched:sched_vmf_rundelay//num="rundelay/1000"/alias=rundelay(us)/', '-i', '1000'])
     for std, line in num_dist.run(runtime, memleak_check):
         if not memleak_check or (
             std == PerfProf.STDERR and not PerfProf.lost_events(line)):
