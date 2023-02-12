0da483f8c8a78c219f5418ae9809174aa823d929
duanery <corcpp@foxmail.com>
2023-02-12 16:10:33 +0800

    tests: irq-off test cases
    Signed-off-by: duanery <corcpp@foxmail.com>

---
+++ tests/test_irq-off.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from PerfProf import PerfProf
+
+def test_irq_off1(runtime, memleak_check):
+    #perf-prof irq-off --period 10ms --than 20ms -g
+    prof = PerfProf(["irq-off", '--period', '10ms', '--than', '20ms', '-g'])
+    for std, line in prof.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
+
+def test_irq_off2(runtime, memleak_check):
+    #perf-prof irq-off --period 10ms --than 20ms -g --exclude-user
+    prof = PerfProf(["irq-off", '--period', '10ms', '--than', '20ms', '-g', '--exclude-user'])
+    for std, line in prof.run(runtime, memleak_check):
+        if not memleak_check or (
+            std == PerfProf.STDERR and not PerfProf.lost_events(line)):
+            print(line, end='', flush=True)
+        if not memleak_check:
+            assert std == PerfProf.STDOUT
