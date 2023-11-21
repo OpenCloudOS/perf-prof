9f7006f11a765005661975f4213f58bcd39d1902
duanery <corcpp@foxmail.com>
2023-11-21 18:42:33 +0800

    signal: Refactor and support prof_dev.
    Signed-off-by: duanery <corcpp@foxmail.com>

--- signal.c
+++ signal.c
@@ -10,35 +10,28 @@
 #include <trace_helpers.h>
 #include <stack_helpers.h>
 
-
-struct monitor monitor_signal;
-static void signal_sample_callchain(union perf_event *event, int instance);
-static struct monitor_ctx {
-    struct callchain_ctx *cc;
-    struct env *env;
-} ctx;
-static int monitor_ctx_init(struct env *env)
+static int monitor_ctx_init(struct prof_dev *dev)
 {
     tep__ref();
-    if (env->callchain) {
-        ctx.cc = callchain_ctx_new(callchain_flags(CALLCHAIN_KERNEL), stdout);
-        monitor_signal.pages *= 2;
-        monitor_signal.sample = signal_sample_callchain;
+    if (dev->env->callchain) {
+        dev->private = callchain_ctx_new(callchain_flags(dev, CALLCHAIN_KERNEL), stdout);
+        dev->pages *= 2;
     }
-    ctx.env = env;
     return 0;
 }
 
-static void monitor_ctx_exit(void)
+static void monitor_ctx_exit(struct prof_dev *dev)
 {
-    if (ctx.env->callchain) {
-        callchain_ctx_free(ctx.cc);
+    if (dev->env->callchain) {
+        callchain_ctx_free(dev->private);
     }
     tep__unref();
 }
 
-static int signal_init(struct perf_evlist *evlist, struct env *env)
+static int signal_init(struct prof_dev *dev)
 {
+    struct perf_evlist *evlist = dev->evlist;
+    struct env *env = dev->env;
     struct perf_event_attr attr = {
         .type          = PERF_TYPE_TRACEPOINT,
         .config        = 0,
@@ -49,14 +42,14 @@ static int signal_init(struct perf_evlist *evlist, struct env *env)
         .read_format   = 0,
         .pinned        = 1,
         .disabled      = 1,
-        .exclude_callchain_user = exclude_callchain_user(CALLCHAIN_KERNEL),
-        .exclude_callchain_kernel = exclude_callchain_kernel(CALLCHAIN_KERNEL),
+        .exclude_callchain_user = exclude_callchain_user(dev, CALLCHAIN_KERNEL),
+        .exclude_callchain_kernel = exclude_callchain_kernel(dev, CALLCHAIN_KERNEL),
         .wakeup_events = 1,
     };
     struct perf_evsel *evsel;
     int id;
 
-    if (monitor_ctx_init(env) < 0)
+    if (monitor_ctx_init(dev) < 0)
         return -1;
 
     id = tep__event_id("signal", "signal_generate");
@@ -72,11 +65,14 @@ static int signal_init(struct perf_evlist *evlist, struct env *env)
     return 0;
 }
 
-static int signal_filter(struct perf_evlist *evlist, struct env *env)
+static int signal_filter(struct prof_dev *dev)
 {
+    struct perf_evlist *evlist = dev->evlist;
+    struct env *env = dev->env;
     char filter[64];
     struct perf_evsel *evsel;
     int err;
+
     if (env->filter) {
         perf_evlist__for_each_evsel(evlist, evsel) {
             snprintf(filter, sizeof(filter), "comm~\"%s\"", env->filter);
@@ -88,37 +84,12 @@ static int signal_filter(struct perf_evlist *evlist, struct env *env)
     return 0;
 }
 
-static void signal_exit(struct perf_evlist *evlist)
+static void signal_exit(struct prof_dev *dev)
 {
-    monitor_ctx_exit();
+    monitor_ctx_exit(dev);
 }
 
-static void signal_sample(union perf_event *event, int instance)
-{
-    // in linux/perf_event.h
-    // PERF_SAMPLE_TID | PERF_SAMPLE_TIME | PERF_SAMPLE_CPU | PERF_SAMPLE_RAW
-    struct sample_type_data {
-        struct {
-            __u32    pid;
-            __u32    tid;
-        }    tid_entry;
-        __u64   time;
-        struct {
-            __u32    cpu;
-            __u32    reserved;
-        }    cpu_entry;
-        struct {
-            __u32   size;
-	        __u8    data[0];
-        } raw;
-    } *data = (void *)event->sample.array;
-
-    tep__update_comm(NULL, data->tid_entry.tid);
-    print_time(stdout);
-    tep__print_event(data->time/1000, data->cpu_entry.cpu, data->raw.data, data->raw.size);
-}
-
-static void signal_sample_callchain(union perf_event *event, int instance)
+static void signal_sample(struct prof_dev *dev, union perf_event *event, int instance)
 {
     // in linux/perf_event.h
     // PERF_SAMPLE_TID | PERF_SAMPLE_TIME | PERF_SAMPLE_CPU | PERF_SAMPLE_RAW
@@ -137,23 +108,25 @@ static void signal_sample_callchain(union perf_event *event, int instance)
     struct {
         __u32   size;
         __u8    data[0];
-    } *raw = (void *)data->callchain.ips + data->callchain.nr * sizeof(__u64);
+    } *raw = (void *)&data->callchain;
+
+    if (dev->env->callchain)
+        raw = (void *)data->callchain.ips + data->callchain.nr * sizeof(__u64);
 
     tep__update_comm(NULL, data->tid_entry.tid);
     print_time(stdout);
     tep__print_event(data->time/1000, data->cpu_entry.cpu, raw->data, raw->size);
-    if (ctx.env->callchain) {
-        print_callchain_common(ctx.cc, &data->callchain, data->tid_entry.pid);
-    }
+    if (dev->env->callchain)
+        print_callchain_common(dev->private, &data->callchain, data->tid_entry.pid);
 }
 
 
 static const char *signal_desc[] = PROFILER_DESC("signal",
     "[OPTION...] [--filter comm] [-g]",
     "Demo", "",
-    "TRACEPOINT", "",
+    "TRACEPOINT",
     "    signal:signal_generate", "",
-    "EXAMPLES", "",
+    "EXAMPLES",
     "    "PROGRAME" signal --filter python");
 static const char *signal_argv[] = PROFILER_ARGV("signal",
     PROFILER_ARGV_OPTION,
@@ -170,5 +143,5 @@ struct monitor monitor_signal = {
     .comm   = monitor_tep__comm,
     .sample = signal_sample,
 };
-MONITOR_REGISTER(monitor_signal)
+PROFILER_REGISTER(monitor_signal)
 
