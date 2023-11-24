04ac73eb27f3f296eb51417641823402542d889c
duanery <corcpp@foxmail.com>
2023-11-24 16:01:37 +0800

    split-lock: Added display of ip, time, callchain, run%, etc.
    Signed-off-by: duanery <corcpp@foxmail.com>

--- split-lock.c
+++ split-lock.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/mman.h>
+#include <linux/time64.h>
 #include "monitor.h"
+#include "stack_helpers.h"
 
-struct monitor split_lock;
 
 /******************************************************
 split-lock test
@@ -32,9 +33,14 @@ static void *do_split_lock(void *unused) {
 split-lock ctx
 ******************************************************/
 struct split_lock_ctx {
-    int nr_cpus;
-    uint64_t *counter;
-    uint64_t *polling;
+    int nr_ins;
+    struct lock_info {
+        uint64_t counter; // sample counter
+        uint64_t polling; // read
+        uint64_t ena;
+        uint64_t run;
+    } *p;
+    struct callchain_ctx *cc;
 };
 
 static void monitor_ctx_exit(struct prof_dev *dev);
@@ -44,13 +50,14 @@ static int monitor_ctx_init(struct prof_dev *dev)
     if (!ctx)
         return -1;
     dev->private = ctx;
-    ctx->nr_cpus = get_present_cpus();
-    ctx->counter = calloc(ctx->nr_cpus, sizeof(uint64_t));
-    if (!ctx->counter)
+    ctx->nr_ins = prof_dev_nr_ins(dev);
+    ctx->p = calloc(ctx->nr_ins, sizeof(*ctx->p));
+    if (!ctx->p)
         goto failed;
-    ctx->polling = calloc(ctx->nr_cpus, sizeof(uint64_t));
-    if (!ctx->polling)
-        goto failed;
+
+    if (dev->env->callchain) {
+        ctx->cc = callchain_ctx_new(callchain_flags(dev, CALLCHAIN_KERNEL | CALLCHAIN_USER), stdout);
+    }
     return 0;
 
 failed:
@@ -61,10 +68,8 @@ failed:
 static void monitor_ctx_exit(struct prof_dev *dev)
 {
     struct split_lock_ctx *ctx = dev->private;
-    if (ctx->counter)
-        free(ctx->counter);
-    if (ctx->polling)
-        free(ctx->polling);
+    if (ctx->p)
+        free(ctx->p);
     free(ctx);
 }
 
@@ -77,11 +82,14 @@ static int split_lock_init(struct prof_dev *dev)
         .config      = 0x10f4,   //split_lock, Intel
         .size        = sizeof(struct perf_event_attr),
         .sample_period = env->trigger_freq,  //每trigger_freq个split_lock发起一个PMI中断, 发起1个采样.
-        .sample_type = PERF_SAMPLE_TID | PERF_SAMPLE_CPU | PERF_SAMPLE_READ,
-        .read_format = 0,
-        .exclude_host = env->exclude_host,  //是否只采样guest模式.
-        .pinned        = 0,
+        .sample_type = PERF_SAMPLE_IP | PERF_SAMPLE_TID | PERF_SAMPLE_TIME | PERF_SAMPLE_CPU | PERF_SAMPLE_READ |
+                       (env->callchain ? PERF_SAMPLE_CALLCHAIN : 0),
+        .read_format = PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING,
+        .pinned      = 0,
         .disabled    = 1,
+        .exclude_host = env->exclude_host,  //是否只采样guest模式.
+        .exclude_callchain_user = exclude_callchain_user(dev, CALLCHAIN_KERNEL | CALLCHAIN_USER),
+        .exclude_callchain_kernel = exclude_callchain_kernel(dev, CALLCHAIN_KERNEL | CALLCHAIN_USER),
         .wakeup_events = 1, //1个事件
     };
     struct perf_evsel *evsel;
@@ -134,16 +142,25 @@ static void split_lock_exit(struct prof_dev *dev)
 static int split_lock_read(struct prof_dev *dev, struct perf_evsel *evsel, struct perf_counts_values *count, int instance)
 {
     struct split_lock_ctx *ctx = dev->private;
-    int cpu = prof_dev_ins_cpu(dev, instance);
     uint64_t counter = 0;
+    uint64_t enabled = 0;
+    uint64_t running = 0;
 
-    if (count->val > ctx->polling[cpu]) {
-        counter = count->val - ctx->polling[cpu];
-        ctx->polling[cpu] = count->val;
+    if (count->val > ctx->p[instance].polling) {
+        counter = count->val - ctx->p[instance].polling;
+        ctx->p[instance].polling = count->val;
+    }
+    if (count->ena > ctx->p[instance].ena) {
+        enabled = count->ena - ctx->p[instance].ena;
+        ctx->p[instance].ena = count->ena;
+    }
+    if (count->run > ctx->p[instance].run) {
+        running = count->run - ctx->p[instance].run;
+        ctx->p[instance].run = count->run;
     }
     if (counter) {
         print_time(stdout);
-        printf("cpu %d split-lock %lu\n", cpu, counter);
+        printf("cpu %3d split-lock %lu run %lu%%\n", prof_dev_ins_cpu(dev, instance), counter, running*100/enabled);
     }
     return 0;
 }
@@ -152,27 +169,35 @@ static void split_lock_sample(struct prof_dev *dev, union perf_event *event, int
 {
     struct split_lock_ctx *ctx = dev->private;
     // in linux/perf_event.h
-    // PERF_SAMPLE_TID | PERF_SAMPLE_CPU | PERF_SAMPLE_READ
+    // PERF_SAMPLE_IP | PERF_SAMPLE_TID | PERF_SAMPLE_TIME | PERF_SAMPLE_CPU | PERF_SAMPLE_READ,
     struct sample_type_data {
+        __u64   ip;
         struct {
             __u32    pid;
             __u32    tid;
         }    tid_entry;
+        __u64   time;
         struct {
             __u32    cpu;
             __u32    reserved;
         }    cpu_entry;
         __u64 counter; //split-lock次数
+        __u64 enabled;
+        __u64 running;
+        struct callchain callchain;
     } *data = (void *)event->sample.array;
     uint64_t counter = 0;
 
-    if (data->counter > ctx->counter[data->cpu_entry.cpu]) {
-        counter = data->counter - ctx->counter[data->cpu_entry.cpu];
-        ctx->counter[data->cpu_entry.cpu] = data->counter;
+    if (data->counter > ctx->p[instance].counter) {
+        counter = data->counter - ctx->p[instance].counter;
+        ctx->p[instance].counter = data->counter;
     }
     if (counter) {
         print_time(stdout);
-        printf("cpu %d pid %d tid %d split-lock %lu\n", data->cpu_entry.cpu, data->tid_entry.pid, data->tid_entry.tid, counter);
+        printf("    pid %6d tid %6d [%03d] %llu.%06llu: split-lock: %lu ip %08llx\n", data->tid_entry.pid, data->tid_entry.tid,
+                        data->cpu_entry.cpu, data->time / NSEC_PER_SEC, (data->time % NSEC_PER_SEC)/1000, counter, data->ip);
+        if (dev->env->callchain)
+            print_callchain_common(ctx->cc, &data->callchain, data->tid_entry.pid);
     }
 }
 
@@ -188,8 +213,8 @@ static const char *split_lock_desc[] = PROFILER_DESC("split-lock",
 static const char *split_lock_argv[] = PROFILER_ARGV("split-lock",
     PROFILER_ARGV_OPTION,
     "FILTER OPTION:",
-    "exclude-host",
-    PROFILER_ARGV_PROFILER, "trigger", "perins", "test");
+    "exclude-host", "user-callchain", "kernel-callchain",
+    PROFILER_ARGV_PROFILER, "trigger", "perins", "call-graph", "test");
 struct monitor split_lock = {
     .name = "split-lock",
     .desc = split_lock_desc,
