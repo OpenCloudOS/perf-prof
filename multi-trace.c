OpenCloudOS/perf-prof

commit f7328091f2b8a84b583475c65d504bbc82d37047

multi-trace: The source device determines if its events match the given pid/tid.
Signed-off-by: duanery <corcpp@foxmail.com>

multi-trace.c
@@ -959,6 +959,21 @@ bool event_need_to_print(union perf_event *event1, union perf_event *event2, str
         curr->key == info->key)
         return true;
 
+    if ((env->samepid || env->sametid) && match)
+    if (iter->event->header.type == PERF_RECORD_DEV) {
+        struct perf_record_dev *event_dev = (void *)iter->event;
+        struct prof_dev *source_dev = event_dev->dev;
+        // Use the samepid() of the source device to determine whether the event matches the given pid/tid.
+        if (unlikely(source_dev->forward.samepid)) {
+            if (source_dev->forward.samepid(source_dev, &event_dev->event,
+                env->samepid ? e1->tid_entry.pid : -1, env->sametid ? e1->tid_entry.tid : -1))
+                return true;
+            if (source_dev->forward.samepid(source_dev, &event_dev->event,
+                env->samepid ? e2->tid_entry.pid : -1, env->sametid ? e2->tid_entry.tid : -1))
+                return true;
+        }
+    }
+
     return false;
 }
 

monitor.h
@@ -324,6 +324,9 @@ struct prof_dev {
         struct perf_record_dev *event_dev;
         short tid_pos, time_pos, id_pos, cpu_pos;
         short forwarded_time_pos; // perf_record_dev.time
+        // Whether the events generated by the source device match the pid/tid.
+        // Currently only used within 'task-state' profiler.
+        bool (*samepid)(struct prof_dev *dev, union perf_event *event, int pid, int tid);
     } forward;
 };
 

commit 51113226228a35304452c126bbbcfac5c610153b

multi-trace: e2 may be NULL.
Fixes: f732809 ("multi-trace: The source device determines if its ..")
Signed-off-by: duanery <corcpp@foxmail.com>

multi-trace.c
@@ -968,7 +968,8 @@ bool event_need_to_print(union perf_event *event1, union perf_event *event2, str
             if (source_dev->forward.samepid(source_dev, &event_dev->event,
                 env->samepid ? e1->tid_entry.pid : -1, env->sametid ? e1->tid_entry.tid : -1))
                 return true;
-            if (source_dev->forward.samepid(source_dev, &event_dev->event,
+            if (e2 &&
+                source_dev->forward.samepid(source_dev, &event_dev->event,
                 env->samepid ? e2->tid_entry.pid : -1, env->sametid ? e2->tid_entry.tid : -1))
                 return true;
         }