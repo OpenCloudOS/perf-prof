e88d2f7b202749728f813305f78403079d664034
duanery <corcpp@foxmail.com>
2024-03-06 17:40:01 +0800

    perf-prof 1.1
    Signed-off-by: duanery <corcpp@foxmail.com>

--- monitor.c
+++ monitor.c
@@ -108,7 +108,7 @@ struct env env;
 
 static volatile int running = 0;
 
-const char *main_program_version = PROGRAME " 1.0";
+const char *main_program_version = PROGRAME " 1.1";
 
 enum {
     LONG_OPT_start = 500,
