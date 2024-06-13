OpenCloudOS/perf-prof

commit 75ca4668b1ac09a9228b78965124093a84b2a79e

bash completion: Enhanced --event option.
[Tab] First perform prefix matching and find the matching prefix through `strncmp'.
If not found, perform substring matching again and search through `strstr'. Outputs
the longest matching common prefix event.

[Tab][Tab] performs substring matching and searches through `strstr`. Output all
matching results for selection.

Enhanced scenarios.
   Typing                                      Seeing
-e hrtimer[Tab]                             -e timer:hrtimer_
                                               The longest common prefix containing
                                               "hrtimer" is "timer:hrtimer_".

-e kfree[Tab][Tab]                             kmem:kfree     skb:kfree_skb
-e wakeup[Tab][Tab]                            List events containing "wakeup"

-e wakeup[Tab]                                 Nothing
                                               The longest common prefix does not
                                               contain "wakeup" and is not output.

-e ioctl[menu-complete]                     -e syscalls:sys_enter_ioctl

Signed-off-by: duanery <corcpp@foxmail.com>

monitor.c
@@ -290,6 +290,100 @@ static int parse_arg_cb(const struct option *opt, const char *arg, int unset)
     return parse_arg(opt->short_name, (char *)arg);
 }
 
+static void compgen_events(char **evt_list, int evt_num, void *opaque)
+{
+    struct {
+        char *prefix;
+        const char *match;
+        int skiplen;
+        int comp_type;
+    } *op = opaque;
+    const char *prefix = op->prefix ?: "";
+    char *prev = NULL, *found;
+    int match_len = op->match ? strlen(op->match) : 0;
+    int i, j, matched = 0;
+    unsigned int maxprefix = -1;
+
+    if (match_len == 0) {
+        for (i = 0; i < evt_num; i++)
+            printf("'%s%s'\n", prefix, evt_list[i]);
+        return;
+    }
+
+    /*
+     # COMP_TYPE              Need prefix?   Substring matching?   matched==1
+     # 9 [Tab]                Y              Y (Prefix first)      Output ','
+     # ? [Tab][Tab]           N              Y                     Output ','
+     # % menu-complete        Y              Y                     Output ','
+     # * insert-completions   Y              Y (!prefix)           No output ','
+     #
+     # ! show-all-if-ambiguous   Same as [Tab][Tab]
+     # @ show-all-if-unmodified  Same as [Tab][Tab]
+     */
+
+    // ![Tab]: [Tab][Tab], menu-complete, etc.
+    // substring matching: strstr.
+    if (op->comp_type != 9) {
+        for (i = 0; i < evt_num; i++)
+            if (strstr(evt_list[i], op->match) != NULL) {
+                if (op->comp_type != '*')
+                    printf("'%s%s'\n", prefix, evt_list[i] + op->skiplen);
+                else if (!op->prefix)
+                    printf("'%s'\n", evt_list[i]);
+                found = evt_list[i];
+                matched++;
+            }
+        if (matched == 1 && op->comp_type != '*')
+            printf("'%s%s,'\n", prefix, found + op->skiplen);
+        return;
+    }
+
+    // [Tab] prefix matching: strncmp.
+    for (i = 0; i < evt_num; i++)
+        if (strncmp(evt_list[i], op->match, match_len) == 0)
+            matched++;
+    if (matched != 0) {
+        j = matched;
+        for (i = 0; j; i++)
+            if (strncmp(evt_list[i], op->match, match_len) == 0) {
+                printf("'%s%s'\n", prefix, evt_list[i] + op->skiplen);
+                if (matched == 1)
+                    printf("'%s%s,'\n", prefix, evt_list[i] + op->skiplen);
+                j--;
+            }
+        return;
+    }
+
+    // [Tab] substring matching. The longest common prefix must contain `op->match'.
+    for (i = 0; i < evt_num; i++)
+        if ((found = strstr(evt_list[i], op->match)) != NULL) {
+            matched++;
+            if (prev) {
+                const char *s1 = prev, *s2 = evt_list[i];
+                j = 0; while (*s1++ == *s2++) j++;
+                if (j < maxprefix) maxprefix = j;
+                if (maxprefix < found - evt_list[i] + match_len)
+                    goto failed;
+            }
+            prev = evt_list[i];
+        }
+    if (matched != 0) {
+        j = matched;
+        for (i = 0; j; i++)
+            if (strstr(evt_list[i], op->match) != NULL) {
+                printf("'%s%s'\n", prefix, evt_list[i] + op->skiplen);
+                if (matched == 1)
+                    printf("'%s%s,'\n", prefix, evt_list[i] + op->skiplen);
+                j--;
+            }
+        return;
+    }
+
+failed:
+    printf("'%s%s'\n", prefix, op->match);
+    printf("'%s%s,'\n", prefix, op->match);
+}
+
 static int compgen_arg(const struct option *opt, const char *arg, int comp_type)
 {
     const char *comma;
@@ -300,7 +394,7 @@ static int compgen_arg(const struct option *opt, const char *arg, int comp_type)
     switch (opt->short_name) {
     case 'e':
         comma = strrchr(arg, ',');
-        if (comp_type != '?') {
+        if (comp_type == 9 || comp_type == '%' || comp_type == '*') {
             if (COMP_SKIPLEN) {
                 /*
                  * If option parameter contains "$COMP_WORDBREAKS" characters, it will be separated
@@ -330,16 +424,22 @@ static int compgen_arg(const struct option *opt, const char *arg, int comp_type)
                     skiplen = skip - arg;
             } else if (comma) {
             make_prefix:
-                prefix = malloc(comma - arg + 3);
-                if (prefix) {
-                    prefix[0] = '\'';
-                    strncpy(prefix + 1, arg, comma - arg + 1);
-                    prefix[comma - arg + 2] = '\0';
-                }
+                prefix = strndup(arg, comma - arg + 1);
             }
+        } {
+            struct {
+                char *prefix;
+                const char *match;
+                int skiplen;
+                int comp_type;
+            } op = {
+                .prefix = prefix,
+                .match = comma ? comma + 1 : arg,
+                .skiplen = skiplen,
+                .comp_type = comp_type,
+            };
+            print_tracepoint_events(compgen_events, (void *)&op);
         }
-        if (print_tracepoint_events(prefix ?: "'", comma ? comma + 1 : arg, "'", skiplen) == 1)
-            print_tracepoint_events(prefix ?: "'", comma ? comma + 1 : arg, ",'", skiplen);
         if (prefix) free(prefix);
         break;
     default:

list.c
@@ -51,25 +51,20 @@ static int cmp_string(const void *a, const void *b)
  * Print the events from <debugfs_mount_point>/tracing/events
  */
 
-int print_tracepoint_events(const char *prefix, const char *match, const char *suffix, int match_skiplen)
+void print_tracepoint_events(tracepoint_cb cb, void *opaque)
 {
     DIR *sys_dir, *evt_dir;
     struct dirent *sys_dirent, *evt_dirent;
     char evt_path[MAXPATHLEN];
     char *dir_path;
     char **evt_list = NULL;
-    int match_len = match ? strlen(match) : 0;
     unsigned int evt_i = 0, evt_num = 0;
     bool evt_num_known = false;
-    int matched = 0;
-
-    prefix = prefix ?: "";
-    suffix = suffix ?: "";
 
 restart:
     sys_dir = tracing_events__opendir();
     if (!sys_dir)
-        return -1;
+        return;
 
     if (evt_num_known) {
         evt_list = zalloc(sizeof(char *) * evt_num);
@@ -112,37 +107,37 @@ next:
         goto restart;
     }
     qsort(evt_list, evt_num, sizeof(char *), cmp_string);
-    evt_i = 0;
-    while (evt_i < evt_num) {
-        if (!match || strncmp(evt_list[evt_i], match, match_len) == 0) {
-            printf("%s%s%s\n", prefix, evt_list[evt_i] + match_skiplen, suffix);
-            matched++;
+
+    if (cb)
+        cb(evt_list, evt_num, opaque);
+    else {
+        evt_i = 0;
+        while (evt_i < evt_num) {
+            printf("%s\n", evt_list[evt_i]);
+            evt_i++;
         }
-        evt_i++;
     }
 
 out_free:
-    evt_num = evt_i;
     for (evt_i = 0; evt_i < evt_num; evt_i++)
         zfree(&evt_list[evt_i]);
     zfree(&evt_list);
-    return matched;
+    return;
 
 out_close_evt_dir:
     closedir(evt_dir);
 out_close_sys_dir:
     closedir(sys_dir);
 
+    evt_num = evt_i;
     if (evt_list)
         goto out_free;
-    return matched;
 }
-
 
 static int list_argc_init(int argc, char *argv[])
 {
     setup_pager();
-    print_tracepoint_events(NULL, NULL, NULL, 0);
+    print_tracepoint_events(NULL, NULL);
     exit(0);
 }
 

monitor.h
@@ -547,6 +547,7 @@ struct prof_dev *trace_dev_open(const char *event, struct perf_cpu_map *cpu_map,
 
 
 //list.c
-int print_tracepoint_events(const char *prefix, const char *match, const char *suffix, int match_skiplen);
+typedef void (*tracepoint_cb)(char **evt_list, int evt_num, void *opaque);
+void print_tracepoint_events(tracepoint_cb cb, void *opaque);
 
 #endif

commit 48ab808a704583af262a049c626d96e36e78eb0f

bash completion: -e block:blkrq completion error
Error scenarios.
   Typing                               Seeing
-e block:blkrq[Tab]                  -e block:block:blkrq

Fixes: 75ca466 ("bash completion: Enhanced --event option.")
Signed-off-by: duanery <corcpp@foxmail.com>

monitor.c
@@ -380,8 +380,8 @@ static void compgen_events(char **evt_list, int evt_num, void *opaque)
     }
 
 failed:
-    printf("'%s%s'\n", prefix, op->match);
-    printf("'%s%s,'\n", prefix, op->match);
+    printf("'%s%s'\n", prefix, op->match + op->skiplen);
+    printf("'%s%s,'\n", prefix, op->match + op->skiplen);
 }
 
 static int compgen_arg(const struct option *opt, const char *arg, int comp_type)