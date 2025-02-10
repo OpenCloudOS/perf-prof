b7c67bd2456a60491be74e20ef6ad70cad345879
duanery <corcpp@foxmail.com>
2025-02-10 10:33:01 +0800

    order: heap sort: Avoid too many fixes out-of-order
    For the same instance, fixing the out-of-order is completely interpretable and no
    longer prompts for fixing.
    
    Frequent in:
      perf-prof blktrace -d /dev/sda -i 1000
    
    Signed-off-by: duanery <corcpp@foxmail.com>

--- order.c
+++ order.c
@@ -385,7 +385,8 @@ perf_mmap_fix_out_of_order(struct prof_dev *main_dev, struct prof_dev *dev,
 {
     union perf_event *event = heap_event->event;
 
-    printf("%s: fix out-of-order event %lu(%d) < %lu(%d)\n", dev->prof->name,
+    if (heap_event->ins != popped_ins || main_dev->env->verbose)
+        printf("%s: fix out-of-order event %lu(%d) < %lu(%d)\n", dev->prof->name,
                     heap_event->time, heap_event->ins, popped_time, popped_ins);
 
     if (!heap_event->writable) {
