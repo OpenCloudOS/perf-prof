f9eac0f1d887c8e3e811ed20a842324339097169
duanery <corcpp@foxmail.com>
2025-05-12 12:10:50 +0800

    breakpoint: Decode the MOV instruction to get the written value
    Tracking memory writes through breakpoints, we can actually obtain the written
    values. Different written values can be more helpful for analysis.
    
    For value that will not change, you can directly read the kernel memory address
    using kcore_read(). However, when perf-prof processes the sampling event, it is
    already too late and it is difficult to ensure that the value does not change.
    
    We can read the instruction and decode the memory address(destination operand)
    to be written and the value(source operand) to be written. Since instructions
    rarely change and all general registers are sampled, they can be fully decoded.
    
    This patch only supports decoding MOV instructions, and the destination operand
    is a memory address, and determines whether the memory address is a breakpoint
    address. Because the data write breakpoint is a trap-class exception, breakpoint
    will reported immediately following the execution of the trapping instruction. We
    need to go back to find the instruction boundary. At the same time, the MOV
    instruction that writes to memory does not change any general registers. Finally,
    we can safely decode the source operand.
    
    Signed-off-by: duanery <corcpp@foxmail.com>

--- breakpoint.c
+++ breakpoint.c
@@ -18,6 +18,9 @@
 #else
 #define PERF_REGS_MASK (((1ULL << PERF_REG_X86_64_MAX) - 1) & ~REG_NOSUPPORT)
 #endif
+
+#include <asm/insn.h>
+
 #elif defined(__aarch64__)
 #define PERF_REGS_MASK ((1ULL << PERF_REG_ARM64_MAX) - 1)
 #endif
@@ -34,13 +37,21 @@ struct hw_breakpoint {
     char typestr[4];
 };
 
+struct insn_decode_ctxt {
+    struct hw_breakpoint *bp;
+    u64 addr;
+    u64 data;
+};
+
 static struct hw_breakpoint hwbp[HBP_NUM];
 struct breakpoint_ctx {
     struct hw_breakpoint hwbp[HBP_NUM];
+    struct insn_decode_ctxt ctxt[HBP_NUM];
     struct callchain_ctx *cc;
     struct flame_graph *flame;
     bool print_ip;
     bool ip_sym;
+    bool kcore;
 };
 
 static int monitor_ctx_init(struct prof_dev *dev)
@@ -52,8 +63,13 @@ static int monitor_ctx_init(struct prof_dev *dev)
         return -1;
     dev->private = ctx;
 
-    for (i = 0; i < HBP_NUM; i++)
+    for (i = 0; i < HBP_NUM; i++) {
         ctx->hwbp[i] = hwbp[i];
+        if (ctx->hwbp[i].type == HW_BREAKPOINT_W) {
+            ctx->kcore = 1;
+            ctx->ctxt[i].bp = &ctx->hwbp[i];
+        }
+    }
 
     ctx->print_ip = 1;
     if (env->callchain) {
@@ -70,6 +86,9 @@ static int monitor_ctx_init(struct prof_dev *dev)
         callchain_ctx_config(ctx->cc, 0, 1, 1, 0, 0, '\n', '\n');
     }
 
+    if (ctx->kcore)
+        kcore_ref();
+
     tep__ref();
     return 0;
 }
@@ -78,6 +97,8 @@ static void monitor_ctx_exit(struct prof_dev *dev)
 {
     struct breakpoint_ctx *ctx = dev->private;
     tep__unref();
+    if (ctx->kcore)
+        kcore_unref();
     callchain_ctx_free(ctx->cc);
     flame_graph_output(ctx->flame);
     flame_graph_close(ctx->flame);
@@ -258,6 +279,249 @@ static void print_regs_intr(struct sample_regs_intr *regs_intr, u64 unused)
 #endif
 }
 
+#if defined(__i386__) || defined(__x86_64__)
+
+#define byte_mask(n) ((1UL << 8*(n))-1)
+
+static u64 reg_read(struct sample_regs_intr *regs_intr, int reg)
+{
+    switch (reg)
+    {
+        case 0: return regs_intr->regs[PERF_REG_X86_AX];
+        case 1: return regs_intr->regs[PERF_REG_X86_CX];
+        case 2: return regs_intr->regs[PERF_REG_X86_DX];
+        case 3: return regs_intr->regs[PERF_REG_X86_BX];
+        case 4: return regs_intr->regs[PERF_REG_X86_SP];
+        case 5: return regs_intr->regs[PERF_REG_X86_BP];
+        case 6: return regs_intr->regs[PERF_REG_X86_SI];
+        case 7: return regs_intr->regs[PERF_REG_X86_DI];
+        case 8: return regs_intr->regs[PERF_REG_X86_R8-REG_NOSUPPORT_N];
+        case 9: return regs_intr->regs[PERF_REG_X86_R9-REG_NOSUPPORT_N];
+        case 10: return regs_intr->regs[PERF_REG_X86_R10-REG_NOSUPPORT_N];
+        case 11: return regs_intr->regs[PERF_REG_X86_R11-REG_NOSUPPORT_N];
+        case 12: return regs_intr->regs[PERF_REG_X86_R12-REG_NOSUPPORT_N];
+        case 13: return regs_intr->regs[PERF_REG_X86_R13-REG_NOSUPPORT_N];
+        case 14: return regs_intr->regs[PERF_REG_X86_R14-REG_NOSUPPORT_N];
+        case 15: return regs_intr->regs[PERF_REG_X86_R15-REG_NOSUPPORT_N];
+        default: return regs_intr->regs[PERF_REG_X86_IP];
+    }
+}
+
+static s32 disp(struct insn *insn)
+{
+    return insn->displacement.got ? insn->displacement.value : 0;
+}
+
+static u64 decode_modrm(struct insn *insn, struct sample_regs_intr *regs_intr)
+{
+    u8 rex_prefix = insn->rex_prefix.got ? insn->rex_prefix.bytes[0] : 0;
+    int modrm_reg, index_reg, base_reg, scale;
+    int modrm_mod, modrm_rm;
+    u64 addr = 0UL;
+
+    modrm_reg = ((rex_prefix << 1) & 8); /* REX.R */
+    index_reg = (rex_prefix << 2) & 8; /* REX.X */
+    base_reg = (rex_prefix << 3) & 8; /* REX.B */
+
+    modrm_mod = X86_MODRM_MOD(insn->modrm.bytes[0]);
+    modrm_reg = modrm_reg | X86_MODRM_REG(insn->modrm.bytes[0]);
+    modrm_rm = base_reg | X86_MODRM_RM(insn->modrm.bytes[0]);
+    if (modrm_mod == 3)
+        return 0UL;
+
+    /* 32/64-bit ModR/M decode. */
+    if ((modrm_rm & 7) == 4) {
+        u8 sib = insn->sib.bytes[0];
+        index_reg |= (sib >> 3) & 7;
+        base_reg |= sib & 7;
+        scale = sib >> 6;
+
+        if ((base_reg & 7) == 5 && modrm_mod == 0)
+            addr += disp(insn);
+        else
+            addr += reg_read(regs_intr, base_reg);
+
+        if (index_reg != 4)
+            addr += reg_read(regs_intr, index_reg) << scale;
+    } else if ((modrm_rm & 7) == 5 && modrm_mod == 0) {
+        addr += disp(insn);
+        if (insn->x86_64) // RIP-Relative Addressing
+            addr += reg_read(regs_intr, 16 /* rIP */);
+    } else {
+        base_reg = modrm_rm;
+        addr += reg_read(regs_intr, base_reg);
+    }
+
+    if (modrm_mod == 1 || modrm_mod == 2)
+        addr += disp(insn);
+
+    return addr;
+}
+
+static u64 decode_mem_abs(struct insn *insn)
+{
+    u64 addr;
+
+    addr = insn->moffset2.got ? insn->moffset2.value : 0UL;
+    addr = (addr << 32) | (insn->moffset1.got ? insn->moffset1.value : 0UL);
+
+    return addr;
+}
+
+static u64 decode_opsrc_reg(struct insn *insn, struct sample_regs_intr *regs_intr, bool byteop)
+{
+    int bytes = byteop ? 1 : insn->opnd_bytes;
+    u8 rex_prefix = insn->rex_prefix.got ? insn->rex_prefix.bytes[0] : 0;
+    int highbyte_regs = (rex_prefix == 0) && byteop;
+    int modrm_reg;
+    u64 data;
+
+    modrm_reg = ((rex_prefix << 1) & 8); /* REX.R */
+    modrm_reg = modrm_reg | X86_MODRM_REG(insn->modrm.bytes[0]);
+
+    if (highbyte_regs && modrm_reg >= 4 && modrm_reg < 8)
+        data = reg_read(regs_intr, modrm_reg & 3) >> 8;
+    else
+        data = reg_read(regs_intr, modrm_reg);
+
+    return bytes == 8 ? data : (data & byte_mask(bytes));
+}
+
+static u64 decode_opsrc_Acc(struct insn *insn, struct sample_regs_intr *regs_intr, bool byteop)
+{
+    int bytes = byteop ? 1 : insn->opnd_bytes;
+    u64 data = reg_read(regs_intr, 0); // AL/rAX
+    return bytes == 8 ? data : (data & byte_mask(bytes));
+}
+
+static u64 decode_opsrc_Imm(struct insn *insn, bool byteop, bool sign_extension)
+{
+    int bytes = byteop ? 1 : insn->opnd_bytes;
+    int size = bytes == 8 ? 4 : bytes;
+    u64 data;
+
+    data = insn->immediate2.got ? (s64)insn->immediate2.value : 0UL;
+    data = (data << 32) | (insn->immediate1.got ? (s64)insn->immediate1.value : 0UL);
+
+    if (!sign_extension)
+        data = data & byte_mask(size);
+
+    return data;
+}
+
+static u64 decode_addr(struct insn *insn, struct sample_regs_intr *regs_intr)
+{
+    switch (insn->opcode.bytes[0])
+    {
+        case 0x88: // MOV Eb,Gb
+        case 0x89: // MOV Ev,Gv
+        case 0xC6: // Grp11 Eb,Ib (1A)
+        case 0xC7: // Grp11 Ev,Iz (1A)
+            return decode_modrm(insn, regs_intr);
+        case 0xA2: // MOV Ob,AL
+        case 0xA3: // MOV Ov,rAX
+            return decode_mem_abs(insn);
+        default:
+            return 0UL;
+    }
+}
+
+static u64 decode_data(struct insn *insn, struct sample_regs_intr *regs_intr)
+{
+    int op = insn->opcode.bytes[0];
+    switch (op)
+    {
+        case 0x88: /* MOV Eb,Gb  */ return decode_opsrc_reg(insn, regs_intr, 1);
+        case 0x89: /* MOV Ev,Gv  */ return decode_opsrc_reg(insn, regs_intr, 0);
+        case 0xA2: /* MOV Ob,AL  */ return decode_opsrc_Acc(insn, regs_intr, 1);
+        case 0xA3: /* MOV Ov,rAX */ return decode_opsrc_Acc(insn, regs_intr, 0);
+        case 0xC6: /* Grp11 Eb,Ib (1A) */ return decode_opsrc_Imm(insn, 1, 1);
+        case 0xC7: /* Grp11 Ev,Iz (1A) */ return decode_opsrc_Imm(insn, 0, 1);
+        default: return 0UL;
+    }
+}
+
+static bool supported(struct insn_decode_ctxt *ctxt, struct insn *insn)
+{
+    struct hw_breakpoint *bp = ctxt->bp;
+
+    if (bp->type != HW_BREAKPOINT_W)
+        return false;
+    if (bp->len != 1 && insn->opnd_bytes != bp->len)
+        return false;
+    if (!insn->opcode.got)
+        return false;
+    if (insn->opcode.nbytes != 1) // only one-byte opcode
+        return false;
+    switch (insn->opcode.bytes[0])
+    {
+        case 0x88: // MOV Eb,Gb.        Move r8 to r/m8.
+        case 0xA2: // MOV Ob,AL.        Move AL to (seg:offset)
+            if (bp->len != 1) return false;
+        case 0x89: // MOV Ev,Gv.        Move r64 to r/m64.
+        case 0xA3: // MOV Ov,rAX.       Move RAX to (offset)
+            break;
+        case 0xC6: // Grp11 Eb,Ib (1A). Move imm8 to r/m8
+        case 0xC7: // Grp11 Ev,Iz (1A). Move imm32 to r/m32
+            if (insn->modrm.got) {
+                int opext = X86_MODRM_REG(insn->modrm.bytes[0]);
+                if (opext == 0) { // MOV
+                    if (insn->opcode.bytes[0] == 0xC6 && bp->len != 1)
+                        return false;
+                    break;
+                }
+            }
+        default:
+            return false;
+    }
+    if (!insn->modrm.got || !insn->modrm.nbytes)
+        return false;
+    return true;
+}
+
+static void x86_decode_insn(struct insn_decode_ctxt *ctxt, u64 ip, struct sample_regs_intr *regs_intr)
+{
+    unsigned char in[15];
+    int in_len = sizeof(in);
+    struct insn insn;
+    enum insn_mode mode;
+    int i;
+
+    if (regs_intr->abi == PERF_SAMPLE_REGS_ABI_NONE)
+        return;
+
+    if (kcore_read(ip - in_len /* Trap */, in, in_len) != in_len)
+        return;
+
+    mode = regs_intr->abi == PERF_SAMPLE_REGS_ABI_64 ? INSN_MODE_64 : INSN_MODE_32;
+    for (i = in_len - 1; i >= 0; i--) {
+        if (insn_decode(&insn, in + i, in_len - i, mode) < 0)
+            continue;
+        if (insn.length != in_len - i)
+            continue;
+
+        // Only supports destination memory operand.
+        if (!supported(ctxt, &insn))
+            continue;
+
+        // Decode the memory address of the destination operand.
+        ctxt->addr = decode_addr(&insn, regs_intr);
+        if (ctxt->addr != ctxt->bp->address)
+            continue;
+
+        // Decode the data of the source operand.
+        ctxt->data = decode_data(&insn, regs_intr);
+
+        printf("      INSN: ");
+        for (; i < in_len; i++)
+            printf("%02x ", in[i]);
+        printf(" ADDR: %lx  DATA: %lx\n", ctxt->addr, ctxt->data);
+        break;
+    }
+}
+
+#endif
+
 static void breakpoint_sample(struct prof_dev *dev, union perf_event *event, int instance)
 {
     struct breakpoint_ctx *ctx = dev->private;
@@ -308,6 +572,22 @@ static void breakpoint_sample(struct prof_dev *dev, union perf_event *event, int
 
     if (dev->env->callchain) {
         regs_intr = (struct sample_regs_intr *)&data->callchain.ips[data->callchain.nr];
+
+        if (ctx->kcore && ctx->hwbp[i].type == HW_BREAKPOINT_W) {
+            #if defined(__i386__) || defined(__x86_64__)
+            /*
+             * Decode the instruction and get the value written to data->addr.
+             *
+             * You cannot directly use kcore_read to read the value of data->addr
+             * because it may change. Currently, kcore_read() reads instructions,
+             * which generally do not change, and decode the source operands.
+             */
+            // Instruction breakpoint, Exception Class: Fault.
+            // Data write breakpoint,  Exception Class: Trap.
+            x86_decode_insn(&ctx->ctxt[i], data->ip, regs_intr);
+            #endif
+        }
+
         if (!dev->env->flame_graph)
             print_callchain_common_cbs(ctx->cc, &data->callchain, data->tid_entry.pid, (callchain_cbs)print_regs_intr, NULL, regs_intr);
         else
